4bfe373625593ad107c872e8c5d4b0ead8b5d3eb
terenowwwm <terenowwwm@gmail.com>
2024-06-01 16:42:02 +0600

    Добавил вывод выбранного блюда, количество

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React, {useState} from "react";
+import {useState} from "react";
 import './App.css'
 import OrderDetails from "./components/orderDetails/orderDetails.tsx";
 import AddItems from "./components/addItems/addItems.tsx";
+interface Food{
+    name:string;
+    count:number;
+    price:number;
+}
 const App = () => {
+
     const [foods, setFoods] = useState([
         {img:'https://cdn-icons-png.flaticon.com/512/1046/1046784.png', name: 'Hunburger', price: 80 },
         {img:'https://cdn-icons-png.flaticon.com/512/590/590836.png', name: 'Coffee', price: 70 },
@@ -11,18 +17,35 @@ const App = () => {
         {img:'https://cdn-icons-png.flaticon.com/512/1057/1057509.png', name: 'Fries', price: 45 },
         {img:'https://cdn-icons-png.flaticon.com/512/1149/1149912.png', name: 'Cola', price: 40 }
     ]);
-    const [count, setCount] = useState(0);
-    const edit = (index) =>{
-        console.log(index)
+    const [orders, setOrders] = useState<Food[]>([]);
+    const select = (index:number) =>{
+        const copyFoods = [...foods]
+        const food = copyFoods[index];
+        const findFood = orders.find(order => order.name === food.name);
+        if (findFood){
+            const update = orders.map((order)=> {
+                if(order.name === food.name){
+                    return {...order, count: order.count + 1};
+                }
+                return order;
+            })
+            setOrders(update)
+        }else{
+            setOrders([...orders, {...food, count:1}])
+        }
+
     }
   return (
       <div className="app">
-          <div className='block'>
-              <OrderDetails name={'ddd'} price={count} count={count}></OrderDetails>
+          <div className='block orderDetails'>
+              <h2 className='orderDetailsH2'>Order Datails</h2>
+              {orders.map((order, index) => (
+                  <OrderDetails key={index} name={order.name} count={order.count} price={order.price}></OrderDetails>
+              ))}
           </div>
           <div className='block'>
               {foods.map((food, index) => (
-                  <AddItems img={food.img} key={index} name={food.name} price={food.price} click={() => edit(index)} />
+                  <AddItems img={food.img} key={index} name={food.name} price={food.price} click={() => select(index)} />
               ))}
           </div>
 
--- src/components/orderDetails/orderDetails.tsx
+++ src/components/orderDetails/orderDetails.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import '../../components.css'
 interface Props {
-    img: string;
     name: string;
     count: number;
     price: number;
 }
 const OrderDetails:React.FC<Props> = (props) => {
     return (
-        <div className="orderDetails">
-            <h2 className='orderDetailsH2'>Order Datails</h2>
-            <p>{props.count} {props.name} {props.price}</p>
+        <div>
+            <p>{props.name}  x{props.count} - {props.price} KGS</p>
         </div>
     );
 };
